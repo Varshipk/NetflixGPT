0c3ffb3ad9b43d54270e9586b609019f0ecc5e11
Varshipk <varshipkatiyar@gmail.com>
2024-03-10 23:41:02 +0530

    removed secret key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,17 @@
+
+
 export const NETFLIX_URL=
        "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 export const AVATAR_URL=
 "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e"
 
+
+
 export const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZTI1ZDkwYzQ4NzIwODUxYTA0Yzk2MGYwNjdmMjVmNiIsInN1YiI6IjY1ZThiODQ3ZWE0MjYzMDE0ODIxM2NiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.f91Ju1AF0azXpj9s4gvdbVWC7cDyecbLK9bDBWJOkZo'
+    Authorization: 'Bearer' + process.env.REACT_APP_TMDB_KEY
   }
 };
 
