1d28cf55e01051aaa4f85e57f457251a759e2b30
Varshipk <varshipkatiyar@gmail.com>
2024-04-29 19:45:44 +0530

    change API key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
 
-
 export const NETFLIX_URL=
        "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 export const AVATAR_URL=
@@ -11,7 +10,7 @@ export const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer' + process.env.REACT_APP_TMDB_KEY
+    Authorization: process.env.REACT_APP_TMDB_KEY
   }
 };
 
